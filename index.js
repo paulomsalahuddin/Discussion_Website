f376df83bd71a705ae779023ab4e2045995c232c
aharonp10 <34289376+aharonp10@users.noreply.github.com>
2017-12-05 21:04:03 -0500

    mySQL

--- index.js
+++ index.js
@@ -21,6 +21,14 @@ app.get('/', function(req,res) {
 
 // const blogData = require('./blog_data.json');
 
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_POASS,
+    database: process.env.DB_NAME
+});
+
 app.use(function(req,res) {
     res.status(404).send('Dont know where that is');
 })
