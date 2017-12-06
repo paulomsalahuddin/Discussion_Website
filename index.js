c86d17552f2bf2a9304f1943b5c9b085dec8cc07
aharonp10 <34289376+aharonp10@users.noreply.github.com>
2017-12-05 20:43:23 -0500

    process env PORT

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(function(req,res) {
     res.status(404).send('Dont know where that is');
 })
 
-const listener = app.listen(5000, function () {
+const listener = app.listen(process.env.PORT || 5000, function () {
     console.log(`Your app is listening on port ${listener.address().port}`)
 })
 
